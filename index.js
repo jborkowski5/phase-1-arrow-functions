e13c40537529b225adef9a6ba061924533bf97d8
Joseph <borkowski.joseph@gmail.com>
2023-08-02 19:53:49 -0400

    Complete

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide () {
+  return 2000 / 100; 
+}
+
+const square = (num) => {  
+    return num * num;
+}
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(divide());
+console.log(square(5));
+console.log(add(3, 7));
